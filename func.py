ab6ce9faeaebc5257f499a50b7c315bf6c4dd301
DESKTOP-NUOPUIF\ITPS <bam05157@naver.com>
2021-07-29 21:54:16 +0900

    pandas study

---
+++ func.py
@@ -0,0 +1,15 @@
+def sum(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a=1, b=1):
+    return a * b
+
+def tot(a):
+    s = 0
+    for i in range(1, a+1):
+        s += i
+    return s
+
